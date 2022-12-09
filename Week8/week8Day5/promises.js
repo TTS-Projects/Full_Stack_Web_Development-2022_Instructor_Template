58ea64159d28941e7095b5ee37e48578abcfeb00
garzarobm <garzarobm@gmail.com>
2022-12-09 09:08:49 -0600

    updated promises.js file

--- Week8/week8Day5/promises.js
+++ Week8/week8Day5/promises.js
@@ -77,17 +77,17 @@ fetch('https://javascript.info/article/promise-chaining/user.json')
 
 
 // same as before, but response.json() parses the remote content as JSON
-fetch('https://javascript.info/article/promise-chaining/user.json')
-  .then(response => response.json())
-  .then(user => alert(user.name)); // iliakan, got user name
+// fetch('https://javascript.info/article/promise-chaining/user.json')
+//   .then(response => response.json())
+//   .then(user => alert(user.name)); // iliakan, got user name
 
 
 // Make a request for user.json
-fetch('https://github.com/garzarobm/TTSOctober2/blob/master/Week8/week8Day5/mygithubusers.json')
+fetch('https://javascript.info/article/promise-chaining/user.json')
 // Load it as json
 .then(response => response.json())
 // Make a request to GitHub
-.then(user => fetch(`https://api.github.com/users/`))
+.then(body => fetch(`https://api.github.com/users/${body.name}`))
 // Load the response as json
 .then(response => response.json())
 // Show the avatar image (githubUser.avatar_url) for 3 seconds (maybe animate it)
@@ -97,3 +97,66 @@ fetch('https://github.com/garzarobm/TTSOctober2/blob/master/Week8/week8Day5/mygi
   img.className = "promise-avatar-example";
   document.body.append(img);
 });
+
+async function showAvatar() {
+
+    // read our JSON
+    let response = await fetch('https://javascript.info/article/promise-chaining/user.json');
+    let user = await response.json();
+  
+    // read github user
+    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+    let githubUser = await githubResponse.json();
+  
+    // show the avatar
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "avatar";
+    document.body.append(img);
+  }
+  
+  showAvatar();
+
+
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status == 200) {
+//         return response.json();
+//       } else {
+//         throw new Error(response.status);
+//       }
+//     });
+// }
+
+loadJson('http://httpstat.us/500')
+  .catch(alert); // Error: 500
+
+
+  async function loadJson(url) { // (1)
+    let response = await fetch(url); // (2)
+  
+    if (response.status == 200) {
+      let json = await response.json(); // (3)
+      return json;
+    }
+  
+    throw new Error(response.status);
+  }
+  
+  loadJson('http://httpstat.us/500')
+    .catch(alert); // Error: 500 (4)
+
+
+    async function wait() {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      
+        return 10;
+      }
+      
+      function f() {
+        // shows 10 after 1 second
+        wait().then(result => alert(result));
+      }
+      
+      f();
